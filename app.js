e047fc583df0482a30b75b9bb4c717985e10ba78
Ahmed Bks <lamghari_ahmed@outlook.fr>
2025-02-02 16:56:20 +0100

    added some functionnalities and some styling

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground.model')
+const ejsMate = require('ejs-mate')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
 const app = express()
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -35,6 +39,22 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const { mongoose } = require('mongoose')
 const Campground = require('../models/campground.model')
 const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -11,11 +11,15 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany()
-    for (let i = 0; i<50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 50) + 10
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)}, ${sample(places)}`
+            title: `${sample(descriptors)}, ${sample(places)}`,
+            image: `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/1920/1080`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repellendus obcaecati possimus, iure nulla repudiandae ab dolores id veniam odio aliquam dolore, itaque ut rerum alias vel quam ipsam repellat.',
+            price
         })
         await camp.save()
     }
