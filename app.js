572e07ab3a7ff29de68b2557d75d19c646f71344
Ahmed Bks <lamghari_ahmed@outlook.fr>
2025-02-05 21:37:37 +0100

    restructuring routes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
-const Campground = require('./models/campground.model')
 const ejsMate = require('ejs-mate')
-const catchAsync = require('./utils/catchAsync')
 const methodOverride = require('method-override')
-const { campgroundSchema, reviewSchema } = require('./schemas')
 const ExpressError = require('./utils/ExpressError')
-const Review = require('./models/review.model')
+
+const campgroundRoute = require('./routes/campground.route')
+const reviewRoute = require('./routes/review.route')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
@@ -20,114 +19,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
+
+app.use('/campgrounds', campgroundRoute)
+app.use('/campgrounds/:id/reviews', reviewRoute)
 
 app.get('/', (req, res) => {
     res.render('home')
 })
-
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', { campgrounds })
-}))
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new')
-})
-
-/*
-app.post('/campgrounds', async (req, res, next) => {
-    try {
-        const campground = new Campground(req.body.campground)
-        await campground.save()
-        res.redirect(`/campgrounds/${campground._id}`)
-    } catch (error) {
-        next(error)
-    }
-})*/
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
-    /*const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
-    const {error} = campgroundSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }*/
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-) /* dans un cas d'erreur catchAsync retourne la fonction avec un next(error)
-   ce qui renvoi vers le middleware responsable de la gestion d'erreur de ce 
-   route avec l'erreur passée en argument pour qu'il puisse l'afficher ou autre
-   dans le cas où il n'y a pas d'erreur la fonction s'execute normalement et
-   le catch(next) retourne 'undefined' */
-
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
-    res.render('campgrounds/show', { campground })
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground })
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params
-    const campground = await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    res.redirect(`/campgrounds/${id}`)
-}))
 
 app.all(/(.*)/, (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
---
+++ routes/campground.route.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const router = express.Router()
+const catchAsync = require('../utils/catchAsync')
+const Campground = require('../models/campground.model')
+const { campgroundSchema, reviewSchema } = require('../schemas')
+const ExpressError = require('../utils/ExpressError')
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+router.get('/', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+}))
+
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+/*
+router.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground)
+        await campground.save()
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (error) {
+        next(error)
+    }
+})*/
+
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    /*const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const {error} = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }*/
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+) /* dans un cas d'erreur catchAsync retourne la fonction avec un next(error)
+   ce qui renvoi vers le middleware responsable de la gestion d'erreur de ce 
+   route avec l'erreur passée en argument pour qu'il puisse l'afficher ou autre
+   dans le cas où il n'y a pas d'erreur la fonction s'execute normalement et
+   le catch(next) retourne 'undefined' */
+
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews')
+    res.render('campgrounds/show', { campground })
+}))
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+}))
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+}))
+
+module.exports = router
