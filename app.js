330a79dfe58a9788437c3b07fab9cf405d49e9bc
Ahmed Bks <lamghari_ahmed@outlook.fr>
2025-02-06 17:01:21 +0100

    setup success/error flashing

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const campgroundRoute = require('./routes/campground.route')
 const reviewRoute = require('./routes/review.route')
+const { Session } = require('inspector/promises') // ?
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
@@ -18,8 +21,26 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
 
 app.use('/campgrounds', campgroundRoute)
 app.use('/campgrounds/:id/reviews', reviewRoute)
--- routes/campground.route.js
+++ routes/campground.route.js
@@ -53,6 +53,7 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     }*/
     const campground = new Campground(req.body.campground)
     await campground.save()
+    req.flash('success', 'successfully made up a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 })
 ) /* dans un cas d'erreur catchAsync retourne la fonction avec un next(error)
@@ -64,6 +65,10 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews')
+    if (!campground) {
+        req.flash('error', 'Can not find that campground!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground })
 }))
 
@@ -75,6 +80,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    req.flash('success', 'successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
